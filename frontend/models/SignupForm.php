af3cb89717e91eb531cee4c223c5c5582a34322f
Synthetica <syntheticafreon@gmail.com>
2016-04-26 08:42:13 +0300

    Some refactor

--- frontend/models/SignupForm.php
+++ frontend/models/SignupForm.php
@@ -20,18 +20,16 @@ class SignupForm extends Model
     public function rules()
     {
         return [
-            ['username', 'filter', 'filter' => 'trim'],
-            ['username', 'required'],
+            [['username', 'email', 'password'], 'required'],
+            [['username', 'email'], 'filter', 'filter' => 'trim'],
+
             ['username', 'unique', 'targetClass' => '\common\models\User', 'message' => 'This username has already been taken.'],
             ['username', 'string', 'min' => 2, 'max' => 255],
 
-            ['email', 'filter', 'filter' => 'trim'],
-            ['email', 'required'],
             ['email', 'email'],
             ['email', 'string', 'max' => 255],
             ['email', 'unique', 'targetClass' => '\common\models\User', 'message' => 'This email address has already been taken.'],
 
-            ['password', 'required'],
             ['password', 'string', 'min' => 6],
         ];
     }
