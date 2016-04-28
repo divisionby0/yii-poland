e4ba1e6baa7164352e601cf09b8535169b224eb7
Vladyslav Lisnyy <vladislav.lisniy@hyuna.bb>
2016-04-28 14:43:22 +0300

    Add insert data to migrations

--- console/migrations/m160426_055929_create_status.php
+++ console/migrations/m160426_055929_create_status.php
@@ -11,6 +11,20 @@ class m160426_055929_create_status extends Migration
             'status_name' => $this->string(45)->notNull(),
             'status_value' => $this->integer(6)->notNull()
         ]);
+
+        // Insert data
+        $this->insert('status', [
+            'status_value' => 1,
+            'status_name' => 'Blacklisted'
+        ]);
+        $this->insert('status', [
+            'status_value' => 5,
+            'status_name' => 'Pending'
+        ]);
+        $this->insert('status', [
+            'status_value' => 10,
+            'status_name' => 'Active'
+        ]);
     }
 
     public function down()
--- console/migrations/m160426_060254_create_user_type.php
+++ console/migrations/m160426_060254_create_user_type.php
@@ -11,6 +11,16 @@ class m160426_060254_create_user_type extends Migration
             'user_type_name' => $this->string(45)->notNull(),
             'user_type_value' => $this->integer(11)->notNull()
         ]);
+
+        // Insert data
+        $this->insert('user_type', [
+            'user_type_value' => 10,
+            'user_type_name' => 'Free'
+        ]);
+        $this->insert('user_type', [
+            'user_type_value' => 30,
+            'user_type_name' => 'Paid'
+        ]);
     }
 
     public function down()
