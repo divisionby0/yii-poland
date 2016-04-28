c8c53b6be1456f3f34a523079128f8c00f4771d8
Vladyslav Lisnyy <vladislav.lisniy@hyuna.bb>
2016-04-28 13:00:51 +0300

    Record Helpers

---
+++ common/helpers/RecordHelpers.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace common\helpers;
+
+use yii;
+
+class RecordHelpers
+{
+    public static function userHas($model_name)
+    {
+        $connection = \Yii::$app->db;
+        $user_id = Yii::$app->user->identity->id;
+        $sql = "SELECT id FROM $model_name WHERE user_id=:user_id";
+        $command = $connection->createCommand($sql);
+        $command->bindValue(':user_id', $user_id);
+        $result = $command->queryOne();
+
+        if ($result == null) {
+            return false;
+        } else {
+            return $result['id'];
+        }
+    }
+}
